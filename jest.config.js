42805eba6bb440ffb19321e9af553934d417b4ee
Simen A. W. Olsen <so@bjerk.io>
2023-05-14 20:30:36 +0200

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
+  testEnvironment: 'node',
   testPathIgnorePatterns: [
     '.*.utils.ts',
     '.*/fixtures.ts',
@@ -7,9 +8,16 @@ module.exports = {
     '.*/dist/.*',
     '.*dist.*',
   ],
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.jest.json',
+  collectCoverageFrom: ['src/**/{!(fixtures),}.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
     },
+  },
+  transform: {
+    '\\.[jt]sx?$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
   },
 };
